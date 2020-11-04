3b54da8ef7d2fb917ad343262bda451eaa17ba67
jasonchenzehui@gmail.com <jasonchenzehui@gmail.com>
2020-11-04 20:03:52 +0800

    d

---
+++ README.md
@@ -0,0 +1,3 @@
+# 自用 taro-tpl for weapp
+
+vue3 + vuex + vant-weapp
--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import './assets/less/index.less'
 
 const App = createApp({
   async onShow(options) {
-    console.log('App Show')
+    console.log('App Show', JSON.stringify(options))
   },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
