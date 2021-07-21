2e84cc2e37ed84158e041692d39bb4b9aacdc6f4
chenzehui <jasonchenzehui@gmail.com>
2021-07-21 12:21:14 +0800

    feat: 更新demo

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,17 +1,6 @@
 <template>
   <view class="home">
-    <van-cell-group>
-      <van-cell title="单元格" value="内容"/>
-      <van-cell title="单元格" value="内容" label="描述信息"/>
-    </van-cell-group>
-    <view class="van-clearfix">
-      <view class="fl">
-        <van-button type="primary">主要按钮{{name}}</van-button>
-      </view>
-      <view class="fr">
-        <van-button type="danger">危险按钮</van-button>
-      </view>
-    </view>
+    <nut-button type="primary">点我</nut-button>
   </view>
 </template>
 
