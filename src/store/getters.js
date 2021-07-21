dd2cafb23509a968498ef357df7ae78b9de9b202
chenzehui <jasonchenzehui@gmail.com>
2021-07-21 12:21:46 +0800

    feat: store自动引入modules，默认开启namespaced

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,13 +1,8 @@
 const getters = {
-  // app
-  loading: state => state.app.loading,
-
   // user
   avatar: state => state.user.avatar,
   tel: state => state.user.tel,
   name: state => state.user.name,
-  logged: state => state.user.logged,
-
 }
 
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,7 +2,6 @@ const state = {
   avatar: 'https://mall.s.maizuo.com/e915e2c990c30f8efa9e30bc75da41ee.jpg',
   tel: '15800066380',
   name: '陈泽辉',
-  logged: 'chan',
 }
 
 const mutations = {
