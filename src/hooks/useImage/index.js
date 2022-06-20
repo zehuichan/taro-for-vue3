1314de6ae531c10e065fcc55ab9ad75c39bcd475
Zehui Chan <jasonchenzehui@gmail.com>
2022-06-20 18:02:41 +0800

    feat: add useImage hook

--- src/hooks/useImage/index.js
+++ src/hooks/useImage/index.js
@@ -1,1 +1,34 @@
-export default function useImage() {}
+import { chooseImage } from '@tarojs/taro'
+import { ref } from 'vue'
+
+export default function useImage(options) {
+  const fileInfo = ref({})
+
+  async function chooseImageAsync(option) {
+    const { count, sizeType, sourceType } = options
+    const finalOptions = Object.assign(
+      {},
+      Object.fromEntries(
+        [
+          ['count', count],
+          ['sizeType', sizeType],
+          ['sourceType', sourceType]
+        ].filter((v) => v[1]) || []
+      ),
+      option || {}
+    )
+
+    const res = await chooseImage({
+      ...finalOptions
+    })
+    fileInfo.value = res
+    console.log(fileInfo.value)
+  }
+
+  return [
+    fileInfo,
+    {
+      choose: chooseImageAsync
+    }
+  ]
+}
