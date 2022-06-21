cec7b3c1e84168caff6c5f38423fc119b34253d7
Zehui Chan <jasonchenzehui@gmail.com>
2022-06-21 16:55:10 +0800

    feat: add useTimeoutFn demo

---
+++ src/hooks/tryOnScopeDispose/index.js
@@ -0,0 +1,9 @@
+import { getCurrentScope, onScopeDispose } from 'vue'
+
+export default function tryOnScopeDispose(fn) {
+  if (getCurrentScope()) {
+    onScopeDispose(fn)
+    return true
+  }
+  return false
+}
---
+++ src/pages/useTimeoutFn/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>{{ text }}</view>
+  <nut-button
+    type="info"
+    shape="square"
+    :disabled="isPending"
+    block
+    @click="restart"
+  >
+    Restart
+  </nut-button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTimeoutFn } from '@/hooks'
+
+const defaultText = `Please wait for 3 seconds`
+const text = ref(defaultText)
+const { isPending, start } = useTimeoutFn(() => {
+  text.value = 'Fired!'
+}, 3000)
+
+const restart = () => {
+  text.value = defaultText
+  start()
+}
+</script>
