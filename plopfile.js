f4fba55e91d08f22925db4867e675eaa7f6904e9
Zehui Chan <jasonchenzehui@gmail.com>
2022-07-14 14:12:40 +0800

    feat: 精简代码

--- plopfile.js
+++
@@ -1,9 +0,0 @@
-const pageGenerator = require('./template/page/prompt')
-const componentGenerator = require('./template/component/prompt')
-const storeGenerator = require('./template/store/prompt.js')
-
-module.exports = function(plop) {
-  plop.setGenerator('page', pageGenerator)
-  plop.setGenerator('component', componentGenerator)
-  plop.setGenerator('store', storeGenerator)
-}
