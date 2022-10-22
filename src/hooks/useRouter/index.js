854eb70549c58a21ad72cb00222da74ed37ae06b
zehuichan <jasonchenzehui@gmail.com>
2022-10-22 11:31:10 +0800

    feat: 新增useRouter

---
+++ src/hooks/useRouter/index.js
@@ -0,0 +1,69 @@
+import {
+  useRouter as useTaroRouter,
+  switchTab as switchTaroTab,
+  reLaunch as relaunchAsync,
+  redirectTo as redirectToAsync,
+  navigateTo,
+  navigateBack as navigateBackAsync,
+  navigateToMiniProgram,
+  navigateBackMiniProgram,
+  openEmbeddedMiniProgram,
+  exitMiniProgram
+} from '@tarojs/taro'
+import { ref } from 'vue'
+
+import { useFrom } from '..'
+import { stringfiyUrl } from '../utils'
+
+export default function useRouter() {
+  const router = ref(useTaroRouter())
+  const from = useFrom()
+
+  function navigate(url, payload, mini, embedded) {
+    if (mini) {
+      const { params = {}, ...extendsPayload } = payload
+      const navigateURI = stringfiyUrl(url, params)
+      const options = { path: navigateURI, ...extendsPayload }
+      return embedded
+        ? openEmbeddedMiniProgram(options)
+        : navigateToMiniProgram(options)
+    }
+    const navigateURI = stringfiyUrl(url, payload)
+    return navigateTo({ url: navigateURI })
+  }
+
+  function switchTab(url) {
+    return switchTaroTab({ url })
+  }
+
+  function relaunch(url, payload) {
+    const navigateURI = stringfiyUrl(url, payload)
+    return relaunchAsync({ url: navigateURI })
+  }
+
+  function redirect(url, payload) {
+    const navigateURI = stringfiyUrl(url, payload)
+    return redirectToAsync({ url: navigateURI })
+  }
+
+  function back(payload, mini) {
+    if (!mini) {
+      return navigateBackAsync({ delta: payload || 1 })
+    }
+    return navigateBackMiniProgram({
+      extraData: payload || {}
+    })
+  }
+
+  return [
+    { ...router.value, from },
+    {
+      navigate,
+      switchTab,
+      relaunch,
+      redirect,
+      back,
+      exit: exitMiniProgram
+    }
+  ]
+}
---
+++ src/pages/useRouter/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <view> 来源页面{{ routeInfo?.from?.route }} </view>
+  <nut-cell
+    title="跳转TabBar"
+    is-link
+    @click="switchTab('/pages/home/index')"
+  />
+  <nut-cell
+    title="重新打开一个页面"
+    is-link
+    @click="relaunch('/pages/useAuthorize/index')"
+  />
+  <nut-cell
+    title="重定向页面"
+    is-link
+    @click="redirect('/pages/useImage/index')"
+  />
+  <nut-cell
+    title="跳转页面"
+    is-link
+    @click="navigate('/pages/demo/index', { from: 'useRouter' })"
+  />
+  <nut-cell title="返回上一页" is-link @click="back()" />
+  <nut-cell
+    title="跳转小程序"
+    is-link
+    @click="
+      navigate(
+        '/pages/index/index',
+        { appId: '2021001168687581', extraData: { from: 'taro-hooks' } },
+        true
+      )
+    "
+  />
+  <nut-cell
+    title="半屏打开小程序"
+    is-link
+    @click="
+      navigate(
+        '/pages/index/index',
+        { appId: '2021001168687581', extraData: { from: 'taro-hooks' } },
+        true,
+        true
+      )
+    "
+  />
+  <nut-cell title="返回小程序" is-link @click="back({}, true)" />
+  <nut-cell title="退出小程序" is-link @click="exit()" />
+</template>
+
+<script setup>
+import { useRouter } from '@/hooks'
+
+const [routeInfo, { navigate, switchTab, relaunch, redirect, back, exit }] =
+  useRouter()
+</script>
+
+<style lang="less">
+page {
+  background-color: #f2f3f5;
+  padding: 20rpx;
+}
+</style>
